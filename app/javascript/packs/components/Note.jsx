66e8ff9e1505b96047ea7070a7ba428b9c6876fa
Happy Machine <thisisdjfresh@gmail.com>
2018-05-15 00:11:15 +0100

    state set in child

--- app/javascript/packs/components/Note.jsx
+++ app/javascript/packs/components/Note.jsx
@@ -1,39 +1,41 @@
 import React from 'react'
-class Note extends React.Component{
+class Note extends React.Component {
     
-    constructor( props ){
-        super( props )
-        this.state=({
+    constructor ( props ){
+        super ( props )
+        this.state = ({
             opacity:'0',
             color:'green'
         })
     }
 
     onMouseOver = () => {
-        this.setState({color:'red'})   
+        this.setState({ color:'red' })   
     }
+
     onMouseOut = () => {
-        this.setState({color:'green'})       
+        this.setState({ color:'green' })       
     }
 
     refresh = () =>{
-        setTimeout( ( props ) => {
-          if (this.state.color!='red'){
-                var localOpacity = parseFloat( this.state.opacity )
+        setTimeout ( ( props ) => {
+          if ( this.state.color!='red' ){
+                var localOpacity = parseFloat ( this.state.opacity )
                 localOpacity+=0.01
-                this.state.opacity > 1.1 ? this.setState({ opacity:'0' }) : this.setState({ opacity:localOpacity.toString() })
+                this.state.opacity > 1.1 ? this.setState ({ opacity:'0' }) : this.setState ({ opacity: localOpacity.toString() })
             }
-            this.refresh()
+            this.refresh ()
          }, 100);  
     }
-      componentDidMount(){
-         this.refresh()
+      
+    componentDidMount () {
+         this.refresh ()
     }
 
     render () {
         return (
             // seperate new line seperate text into spans
-            this.props.content.split('\\n').map(function(line, key) {        
+            this.props.content.split ( '\\n' ).map ( function ( line, key ) {        
                 return (
                     <span key={key} style={this.state} onMouseOver={this.onMouseOver} onMouseOut={this.onMouseOut}>
                         {line}

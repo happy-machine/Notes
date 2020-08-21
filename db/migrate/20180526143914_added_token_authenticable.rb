f3ebe83d01df01eb3e12e8fe99c26d6080436eca
Dan Stein <dan@dyniumrobot.com>
2020-08-21 23:21:35 +0100

    chore(all): clean up

--- db/migrate/20180526143914_added_token_authenticable.rb
+++
@@ -1,8 +0,0 @@
-class AddedTokenAuthenticable < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :authentication_token, :text
-    add_column :users, :authentication_token_created_at, :datetime
-
-    add_index :users, :authentication_token, unique: true
-  end
-end
